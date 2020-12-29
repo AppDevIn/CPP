e710b72ee57a0368ec0541ab02ab67cb162feee4
AppDevIn <teamprojectlive@gmail.com>
2020-12-29 17:33:48 +0800

    ✨ Comparisons with search and Binary Search

--- Week_08/practice.cpp
+++ Week_08/practice.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int search(int arr[], int n, int target);
 int binarySearch(int dataArray[], int arraySize, int target);
 
+static int comparsionsB = 0;
+static int comparsionsS = 0;
+
 int main(int argc, char const *argv[])
 {
     int dataArray[1000];
@@ -13,14 +16,28 @@ int main(int argc, char const *argv[])
     {
         dataArray[(i - 2) / 2] = i;
     }
-
-    cout << "Enter the target value: ";
-    int target;
-    cin >> target;
-
     int len = sizeof(dataArray) / sizeof(dataArray[0]);
-    cout << ((binarySearch(dataArray, len, target) == -1) ? "Not Found" : "Found") << endl;
+    int target = 1;
 
+    while (target != 0)
+    {
+        cout << "Enter the target value: ";
+        cin >> target;
+
+        cout << "" << endl;
+
+        cout << ((binarySearch(dataArray, len, target) == -1) ? "Not Found" : "Found") << endl;
+        cout << "Binary Comparsions: " << comparsionsB << endl;
+
+        cout << "" << endl;
+
+        cout << ((search(dataArray, len, target) == -1) ? "Not Found" : "Found") <<  endl;;
+        cout << "Search Comparsions: " << comparsionsS << endl;
+
+        comparsionsB = 0;
+        comparsionsS = 0;
+
+    }
     return 0;
 }
 
@@ -30,6 +47,7 @@ int search(int arr[], int n, int target)
     int count = 0;
     while (count != n)
     {
+        comparsionsS++;
         if (arr[count] == target)
             return count;
         else if (arr[count] > target)
@@ -46,10 +64,9 @@ int binarySearch(int dataArray[], int arraySize, int target)
     int left = 0;
     int right = arraySize - 1;
 
-
-
     while (left <= right)
     {
+        comparsionsB++;
         int mid = (left + right) / 2;
 
         if (dataArray[mid] == target)
